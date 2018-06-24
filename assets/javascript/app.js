fbe79140befd8c6e4d607e7feb4130c9d53ee158
Shane Henry <k5blazin@gmail.com>
2018-06-24 16:20:47 -0600

    newtemplate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,74 @@
+$(document).ready( function() {
+
+	
+	//button on click event that shows time and starts game when I click start game
+	------
+	//button onclick
+	function clickTimerButton (){
+		$("#clickTimerButton").click(function(){
+        alert("button");
+		}
+);}}
+
+	//function that starts timer when I click start game
+	function timerStart() {
+		
+		(timerStart(){
+  			var counter = 30;
+  			setInterval(function() {
+    		counter--;
+    		
+			if (counter >= 0) {
+      		span = document.getElementById("count");
+      		span.innerHTML = counter;
+    }
+    		if (counter === 0) {
+        	alert('sorry, out of time');
+        	clearInterval(counter);
+    }
+  }, 1000);
+})();
+}
+	}
+
+	//function that displays question
+	function displayQuestion() {
+		
+	}
+
+	//function that displays question options 
+	function displayQuestionOptions() {
+		//code goes here to display choices
+	}
+	
+	//function that checks question answer
+	function displayQuestion() {
+
+	}
+	
+	//function that displays question answer
+	function displayQuestion() {
+
+	}
+	
+	//function that runs through questions and gives appropriate answer options
+	function displayQuestion() {
+
+		// you'll need a loop and if statements
+	}
+	
+	//function that checks win score and produces win/lose html/ reset button
+	function displayQuestion() {
+
+	}
+}
+
 (function() {
   
-	var correctAnswers;
-	var incorrectAnswers;
-	var answerContainer;
-	var totalScore;
+	var correctAnswers = 0;
+	var incorrectAnswers = 0;
+	var answerContainer = 0;
+	var totalScore = 0;
 	
 	
 	var myQuestions = [
@@ -72,70 +137,64 @@
 		 }, 1000);
 });
 
-	//Results
-  	function showResults() {
-	  
-    // gather answers containers from our quiz
-    var answerContainers = quizContainer.querySelectorAll(".answers");
-
-    //track answers
-    let correctAnswers = 0;
-
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-    
-	// find selected answer
-    var answerContainer = answerContainers[questionNumber];
-    var selector = `input[name=question${questionNumber}]:checked`;
-    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-    // if answer is correct
-     if (userAnswer === currentQuestion.correctAnswer) {
-        
-		 // add to the number of correct answers color green
-        numCorrect++; answerContainers[questionNumber].style.color = "lightgreen";
-        
-      } else {
-        // if answer is wrong or blank color answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
-
-  function showSlide(n) {
-    slides[currentSlide].classList.remove("active-slide");
-    slides[n].classList.add("active-slide");
-    currentSlide = n;
-    
-    if (currentSlide === 0) {
-      previousButton.style.display = "none";
-    } else {
-      previousButton.style.display = "inline-block";
-    }
-    
-    if (currentSlide === slides.length - 1) {
-      nextButton.style.display = "none";
-      submitButton.style.display = "inline-block";
-    } else {
-      nextButton.style.display = "inline-block";
-      submitButton.style.display = "none";
-    }
-  }
-
-
-
-  var quizContainer = document.getElementById("quiz");
-  var resultsContainer = document.getElementById("results");
-  var submitButton = document.getElementById("submit");
-
- 
-
-
-  // on submit, show results
-  submitButton.addEventListener("click", showResults);
-  previousButton.addEventListener("click", showPreviousSlide);
-  nextButton.addEventListener("click", showNextSlide);
-})();
+//	//Results
+//  	function showResults() {
+//	  
+//    // gather answers containers from our quiz
+//    var answerContainers = quizContainer.querySelectorAll(".answers");
+//
+//    //track answers
+//    let correctAnswers = 0;
+//
+//    // for each question...
+//    myQuestions.forEach((currentQuestion, questionNumber) => {
+//    
+//	// find selected answer
+//    var answerContainer = answerContainers[questionNumber];
+//    var selector = `input[name=question${questionNumber}]:checked`;
+//    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+//
+//    // if answer is correct
+//     if (userAnswer === currentQuestion.correctAnswer) {
+//        
+//		 // add to the number of correct answers color green
+//        numCorrect++; answerContainers[questionNumber].style.color = "lightgreen";
+//        
+//      } else {
+//        // if answer is wrong or blank color answers red
+//        answerContainers[questionNumber].style.color = "red";
+//      }
+//    });
+//
+//    // show number of correct answers out of total
+//    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+//  }
+//
+//  function showSlide(n) {
+//    slides[currentSlide].classList.remove("active-slide");
+//    slides[n].classList.add("active-slide");
+//    currentSlide = n;
+//    
+//    if (currentSlide === 0) {
+//      previousButton.style.display = "none";
+//    } else {
+//      previousButton.style.display = "inline-block";
+//    }
+//    
+//    if (currentSlide === slides.length - 1) {
+//      nextButton.style.display = "none";
+//      answerCheck.style.display = "inline-block";
+//    } else {
+//      nextButton.style.display = "inline-block";
+//      answerCheck.style.display = "none";
+//    }
+//  }
+//  var quizContainer = document.getElementById("quiz");
+//  var resultsContainer = document.getElementById("results");
+//  var submitButton = document.getElementById("submit");
+//
+//  // on submit, show results
+//  answerCheck.addEventListener("click", showResults);
+//  previousButton.addEventListener("click", showPreviousSlide);
+//  nextButton.addEventListener("click", showNextSlide);
+//})();
